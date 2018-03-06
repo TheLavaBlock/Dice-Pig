f7356923ffaa12c972183ce4ab49d5a1d742cccc
TheLavaBlock <36247472+TheLavaBlock@users.noreply.github.com>
2018-03-06 22:40:24 +0100

    1-4-0 Write game mechanics in script

--- Game.gd
+++ Game.gd
@@ -1,13 +1,77 @@
 extends Node
 
+var dice = 0 # 0 - 5
+var turnPoints = 0 # 0 - 100
+var player1Points = 0 # 0 - 100
+var player2Points = 0 # 0 - 100
+var currentPlayer = 0 # 0 - 1
+
+const colorActive = Color("#ffffff")
+const colorInactive = Color("#bbbbbb")
+
 func _ready():
-	pass
+	randomize()
+
+	resetTurnPoints()
+	resetPlayerPoints()
+	setCurrentPlayer(0)
+
+func resetTurnPoints():
+	turnPoints = 0
+	$Turn/Points.text = str(0)
+
+func resetPlayerPoints():
+	player1Points = 0
+	$Player1/Points.text = str(0)
+	player2Points = 0
+	$Player2/Points.text = str(0)
+	
+func setCurrentPlayer(player):
+	currentPlayer = player
+	if currentPlayer == 0:
+		$Player1/Points.modulate = colorActive
+		$Player2/Points.modulate = colorInactive
+		$Turn/Left.modulate = colorActive
+		$Turn/Left.disabled = false
+		$Turn/Right.modulate = colorInactive
+		$Turn/Right.disabled = true
+	else:
+		$Player1/Points.modulate = colorInactive
+		$Player2/Points.modulate = colorActive
+		$Turn/Left.modulate = colorInactive
+		$Turn/Left.disabled = true
+		$Turn/Right.modulate = colorActive
+		$Turn/Right.disabled = false
+
+func setNextPlayer():
+	resetTurnPoints()
+	if currentPlayer == 0:
+		setCurrentPlayer(1)
+	else:
+		setCurrentPlayer(0)
 
 func _on_DiceButton_pressed():
-	pass
+	dice = randi() % 6
+	$Dice.frame = dice
+	if dice == 0:
+		setNextPlayer()
+		return
+
+	turnPoints += dice + 1
+	$Turn/Points.text = str(turnPoints)
 
 func _on_Left_pressed():
-	pass
+	if turnPoints == 0:
+		return
+
+	player1Points += turnPoints
+	$Player1/Points.text = str(player1Points)
+	setNextPlayer()
 
 func _on_Right_pressed():
-	pass
+	if turnPoints == 0:
+		return
+
+	player2Points += turnPoints
+	$Player2/Points.text = str(player2Points)
+	setNextPlayer()
