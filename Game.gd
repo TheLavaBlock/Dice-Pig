02855fd53124b371fed88abd3a64d5eb865012cf
braincodes <nicki.careers@yahoo.co.uk>
2018-10-29 18:32:51 +0000

    Extend gameplay with endgame and round scoring
    Add endgame function to allow for scoring, and add score-per-round to allow for multiple games

--- Game.gd
+++ Game.gd
@@ -1,25 +1,31 @@
 extends Node
 
-var dice = 0 # 0 - 5
+var dice = 0 # range 0 - 5
 var turnPoints = 0 # 0 - 100
 var player1Points = 0 # 0 - 100
+var player1Total = 0
 var player2Points = 0 # 0 - 100
+var player2Total = 0
 var currentPlayer = 0 # 0 - 1
+var maxPoints = 30
+onready var delay = get_node("Timer")
 
-const colorActive = Color("#ffffff")
-const colorInactive = Color("#bbbbbb")
+const colourActive = Color("#ffffff")
+const colourInactive = Color("#bbbbbb")
+const colourWon = Color("#ff0000")
 
 func _ready():
-	randomize()
-
+	randomize()	# get random numbers for dice
+	
+	# reset game state
 	resetTurnPoints()
 	resetPlayerPoints()
-	setCurrentPlayer(0)
-
+	setCurrentPlayer(0) # set to Player 1
+	
 func resetTurnPoints():
 	turnPoints = 0
 	$Turn/Points.text = str(0)
-
+	
 func resetPlayerPoints():
 	player1Points = 0
 	$Player1/Points.text = str(0)
@@ -29,35 +35,47 @@ func resetPlayerPoints():
 func setCurrentPlayer(player):
 	currentPlayer = player
 	if currentPlayer == 0:
-		$Player1/Points.modulate = colorActive
-		$Player2/Points.modulate = colorInactive
-		$Turn/Left.modulate = colorActive
+		$Player1/Points.modulate = colourActive
+		$Player2/Points.modulate = colourInactive
+		$Turn/Left.modulate = colourActive
 		$Turn/Left.disabled = false
-		$Turn/Right.modulate = colorInactive
+		$Turn/Right.modulate = colourInactive
 		$Turn/Right.disabled = true
 	else:
-		$Player1/Points.modulate = colorInactive
-		$Player2/Points.modulate = colorActive
-		$Turn/Left.modulate = colorInactive
+		$Player1/Points.modulate = colourInactive
+		$Player2/Points.modulate = colourActive
+		$Turn/Left.modulate = colourInactive
 		$Turn/Left.disabled = true
-		$Turn/Right.modulate = colorActive
+		$Turn/Right.modulate = colourActive
 		$Turn/Right.disabled = false
-
+		
 func setNextPlayer():
 	resetTurnPoints()
 	if currentPlayer == 0:
 		setCurrentPlayer(1)
 	else:
 		setCurrentPlayer(0)
-
+		
+func endGame():
+	$Turn/Left.disabled = true
+	$Turn/Right.disabled = true
+	delay.start()
+	yield(delay, "timeout")
+	$Sound/Pig.play()
+	$Turn/Right.modulate = colourInactive
+	$Turn/Left.modulate = colourInactive
+	_ready()
+	
+	
 func _on_DiceButton_pressed():
 	dice = randi() % 6
-	$Dice.frame = dice
+	$Dice.frame = dice # show animation frame for throw number
 	if dice == 0:
 		$Sound/Pig.play()
 		setNextPlayer()
 		return
-
+		
+	# play Dice sounds
 	var diceType = randi() % 4
 	if diceType == 0:
 		$Sound/Dice1.play()
@@ -66,27 +84,38 @@ func _on_DiceButton_pressed():
 	if diceType == 2:
 		$Sound/Dice3.play()
 	if diceType == 3:
-		$Sound/Dice4.play()	
-
+		$Sound/Dice4
 	turnPoints += dice + 1
-	$Turn/Points.text = str(turnPoints)
+	$Turn/Points.text = str(turnPoints) 
 
 func _on_Left_pressed():
 	if turnPoints == 0:
 		return
-
+		
 	$Sound/Hold.play()
-
+		
 	player1Points += turnPoints
 	$Player1/Points.text = str(player1Points)
-	setNextPlayer()
+	if player1Points >= maxPoints:
+		player1Total = player1Total + 1
+		$Player1/Total1.text = str(player1Total)
+		$Player1/Points.modulate = colourWon
+		endGame()
+	else:
+		setNextPlayer()
 
 func _on_Right_pressed():
 	if turnPoints == 0:
 		return
-
+		
 	$Sound/Hold.play()
-
+		
 	player2Points += turnPoints
 	$Player2/Points.text = str(player2Points)
-	setNextPlayer()
+	if player2Points >= maxPoints:
+		player2Total = player2Total + 1
+		$Player2/Total2.text = str(player2Total)
+		$Player2/Points.modulate = colourWon
+		endGame()
+	else:
+		setNextPlayer()
